510d20ee1aad31b55610522551d760cfb0ac92a8
roldel <roldel@protonmail.com>
2021-06-30 00:07:05 +0000

    Initial commit

---
+++ src/components/Results.vue
@@ -0,0 +1,38 @@
+<template>
+  <p>Your reaction time was {{ score }} ms</p>
+  <p class="rank">You ranked {{ rank }} </p>
+
+</template>
+
+<script>
+export default {
+    props: [
+        'score'
+    ],
+    data () {
+        return {
+          rank : null,
+
+        }
+    },
+    mounted() {
+        if (this.score < 250){
+            this.rank = "Ninja fingers"
+        } else if (this.score < 450 ){
+            this.rank = "Rapid reflexes"
+        } else {
+            this.rank = 'Snail pace...'
+        }
+    }
+}
+</script>
+
+<style>
+
+.rank {
+    font-size : 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+}
+
+</style>
